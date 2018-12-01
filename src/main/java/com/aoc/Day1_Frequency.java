9c5449367edb459e1c0e633bbefdf1e6ce15c80e
Akhilaraghavan <akhila.84@gmail.com>
2018-12-01 22:28:10 +0000

    Unused imports

--- src/main/java/com/aoc/Day1_Frequency.java
+++ src/main/java/com/aoc/Day1_Frequency.java
@@ -3,7 +3,6 @@ package com.aoc;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
